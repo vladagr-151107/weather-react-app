304146cfce3170fd4572fcdd2f0650ab1155b07f
vladagr-151107 <vladagrushchenko@gmail.com>
2023-02-06 17:04:12 +0200

    added html and css(but it's not finished yet)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
 }
---
+++ src/Weather.js
@@ -0,0 +1,91 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="container">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <form className="mb-3">
+            <div className="row">
+              <div className="col-2">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  id="current-location"
+                >
+                  Current{" "}
+                </button>
+              </div>
+              <div className="col-8">
+                <input
+                  type="search"
+                  placeholder="Search for a city"
+                  className="form-control"
+                  autoComplete="off"
+                />
+              </div>
+              <div className="col-2">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary"
+                />
+              </div>
+            </div>
+          </form>
+          <div className="overview">
+            <h1> Odesa </h1>
+            <ul>
+              <li>
+                <span> Wednesday, Jan 11 13:53</span>
+              </li>
+              <li> Сlear</li>
+            </ul>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="d-flex weather-temperature">
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                  alt="Clear"
+                />
+                <div className="float-left">
+                  <strong id="temperature"> 2</strong>
+                  <span className="units">°C</span>
+                </div>
+              </div>
+            </div>
+            <div classname="col-6">
+              <ul>
+                <li>
+                  Humidity: <span></span>67%
+                </li>
+                <li>
+                  Wind: <span></span>9 km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <small>
+          <a
+            href="https://github.com/vladagr-151107/vanilla-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code{" "}
+          </a>
+          by{" "}
+          <a
+            href="https://www.instagram.com/_princesska_1511_/?igshid=YmMyMTA2M2Y%3D"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Vlada Grushchenko
+          </a>
+        </small>
+      </div>
+    </div>
+  );
+}
